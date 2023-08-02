8e043f5b6d9dfa583863cb88d06c6081e568f857
sanopopular <prabeshbhandari842@gmail.com>
2023-08-02 20:38:23 +0545

    third commit

--- script.js
+++ script.js
@@ -10,13 +10,29 @@ const form = document.querySelector("form"),
 function checkEmail(){
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if(!emailInput.value.match(emailPattern)){
-        return emailField.classList.add("invalid");
+        return emailField.classList.add("invalid"); //adding invalid class name if the email does not match with emailPattern
     }
-        emailField.classList.remove("invalid");
+        emailField.classList.remove("invalid"); //removing invalid class name if the email value matches with emailPattern
 }
+
+//Hide and show password
+const eyeIcons = document.querySelectorAll(".show-hide");
+
+eyeIcons.forEach(eyeIcons => {
+    eyeIcons.addEventListener("click", () => {
+    const pInput = eyeIcons.parentElement.querySelector("input");
+    if (pInput.type === 'password') {
+        eyeIcons.classList.replace("bx-hide", "bx-show");
+        return (pInput.type = "text");
+    }
+    });
+})
 
 //calling function on form submit
 form.addEventListener("submit", (e) => {
     e.preventDefault(); //preventing form submiting
     checkEmail();
+
+    //calling function on key up
+    emailInput.addEventListener("keyup", checkEmail);
 });
