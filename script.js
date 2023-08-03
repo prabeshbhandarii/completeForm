13f9eb9ea48d6a281895361521eedd30ba97d36b
sanopopular <prabeshbhandari842@gmail.com>
2023-08-03 20:01:22 +0545

    final commit

--- script.js
+++ script.js
@@ -20,19 +20,42 @@ const eyeIcons = document.querySelectorAll(".show-hide");
 
 eyeIcons.forEach(eyeIcons => {
     eyeIcons.addEventListener("click", () => {
-    const pInput = eyeIcons.parentElement.querySelector("input");
+    const pInput = eyeIcons.parentElement.querySelector("input"); // getting the parent element of eye icon and selecting the password input
     if (pInput.type === 'password') {
         eyeIcons.classList.replace("bx-hide", "bx-show");
         return (pInput.type = "text");
     }
+        eyeIcons.classList.replace("bx-show", "bx-hide");
+        return (pInput.type = "password");
     });
-})
+});
+
+// password validation
+function createPass(){
+    const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if(!passInput.value.match(passPattern)){
+        return passField.classList.add("invalid"); // adding invalid class if password input does not match with passPattern
+    }
+    return passField.classList.remove("invalid"); // removing invalid class if password input match the passPattern
+}
+
+// confirming password validation
+function confirmPass(){
+    if(passInput.value !== cPassInput.value || cPassInput.value === ''){
+        return cPassField.classList.add("invalid");
+    }
+    return cPassField.classList.remove("invalid");
+}
 
 //calling function on form submit
 form.addEventListener("submit", (e) => {
     e.preventDefault(); //preventing form submiting
     checkEmail();
+    createPass();
+    confirmPass();
 
     //calling function on key up
     emailInput.addEventListener("keyup", checkEmail);
+    passInput.addEventListener("keyup", createPass);
+    cPassInput.addEventListener("keyup", confirmPass);
 });
